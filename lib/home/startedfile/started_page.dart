9a98ba32740748c591bb495aa757f12624d1142b
Praful Patnecha <prafulpatnecha@gmail.com>
2024-05-14 23:39:28 +0530

    14-05-24

--- lib/home/startedfile/started_page.dart
+++ lib/home/startedfile/started_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:resume_cv/utils/colors.dart';
+import 'package:resume_cv/utils/images_all.dart';
 
 class StartedPage extends StatefulWidget {
   const StartedPage({super.key});
@@ -10,6 +12,36 @@ class StartedPage extends StatefulWidget {
 class _StartedPageState extends State<StartedPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        height: h,
+        width: w,
+        decoration: BoxDecoration(
+          color: onScreenColor,
+          image: DecorationImage(image: backImage, fit: BoxFit.cover),
+        ),
+        child: Column(
+          children: [
+            const Spacer(),
+            Stack(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 17),
+                  height: h/2.4,
+                  width: w,
+                  decoration: BoxDecoration(
+                  color: Colors.white,
+                    borderRadius: BorderRadius.circular(h/40)
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: h/26,),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/utils/images_all.dart
+++ lib/utils/images_all.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/cupertino.dart';
 
 AssetImage logoImage=const AssetImage('assets/images/logoapp.png');
+AssetImage backImage=const AssetImage('assets/images/back.png');
